60667a26e30b764e7518c2e636c08964b67c35be
chrishasbeard <chrisholmes16@gmail.com>
2019-10-17 14:02:56 -0400

    with a foreach loop

---
+++ ForEachAJollyGoodFellow/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ForEachAJollyGoodFellow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aCount = CountLetter("Ali Ababwa");
+            Console.WriteLine("Expected: 4, Actual: " + aCount );
+            int letterCount = CountLetter("Babbabouie", 'b');
+            Console.WriteLine("Expexted: 4, Actual: " + aCount);
+        }
+        public static int CountLetter(string word)
+        {
+            int count = 0;
+            foreach(char letter in word.ToLower())
+            {
+                if(letter == 'a')
+                {
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+        public static int CountLetter(string word, char findLetter)
+        {
+            int count = 0;
+            foreach(char letter in word.ToLower())
+            {
+                if (letter == findLetter)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
